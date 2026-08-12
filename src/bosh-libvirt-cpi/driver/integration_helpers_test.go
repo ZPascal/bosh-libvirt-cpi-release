ec3213d37d38bac460a476638c50642b65706e98
ZPascal <pascal.zimmermann@theiotstudio.com>
2026-08-12 12:26:10 +0200

    test(driver): add URI-driven domain builder and XML helpers for integration tests

---
+++ src/bosh-libvirt-cpi/driver/integration_helpers_test.go
@@ -0,0 +1,45 @@
+//go:build integration
+
+package driver_test
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"bosh-libvirt-cpi/driver"
+	"bosh-libvirt-cpi/driver/domains"
+)
+
+func domBuilderFromEnv() driver.DomainBuilder {
+	uri := os.Getenv("LIBVIRT_URI")
+	u, _ := url.Parse(uri)
+	switch u.Scheme {
+	case "lxc":
+		return domains.LXCDomainBuilder{}
+	case "vbox":
+		return domains.VBoxDomainBuilder{}
+	default:
+		return domains.QEMUDomainBuilder{}
+	}
+}
+
+func testDomainXML(name string) string {
+	uri := os.Getenv("LIBVIRT_URI")
+	u, _ := url.Parse(uri)
+	if u.Scheme == "lxc" {
+		return fmt.Sprintf(`<domain type='lxc'>
+  <name>%s</name>
+  <memory unit='KiB'>65536</memory>
+  <vcpu>1</vcpu>
+  <os><type>exe</type><init>/sbin/init</init></os>
+  <devices><emulator>/usr/lib/libvirt/libvirt_lxc</emulator></devices>
+</domain>`, name)
+	}
+	return fmt.Sprintf(`<domain type='kvm'>
+  <name>%s</name>
+  <memory unit='KiB'>65536</memory>
+  <vcpu>1</vcpu>
+  <os><type arch='x86_64'>hvm</type></os>
+</domain>`, name)
+}
