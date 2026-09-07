ac45fe1d3a154c51f55ae27fad2d6f29d547d661
ZPascal <pascal.zimmermann@theiotstudio.com>
2026-09-07 13:14:24 +0100

    fix(e2e): fix indentation of setpriv args in QEMU start_svc

--- src/bosh-libvirt-cpi/vm/factory.go
+++ src/bosh-libvirt-cpi/vm/factory.go
@@ -738,7 +738,7 @@ func (f Factory) Create(
 					"      env = dict(os.environ)\n" +
 					"      env.update(proc.get('env',{}))\n" +
 					"      setpriv_bin = '/usr/sbin/setpriv' if os.path.exists('/usr/sbin/setpriv') else '/sbin/setpriv'\n" +
-			"      args = [setpriv_bin,'--reuid=1000','--regid=1000','--clear-groups','--'] + args\n" +
+					"      args = [setpriv_bin,'--reuid=1000','--regid=1000','--clear-groups','--'] + args\n" +
 					"      pf = '/var/vcap/sys/run/bpm/'+svc+'/'+svc+'.pid'\n" +
 					"      os.makedirs(os.path.dirname(pf), exist_ok=True)\n" +
 					"      os.chown(os.path.dirname(pf), 1000, 1000)\n" +
